57d18ba25ac5d76cafb657bb46fa05fca6648233
Carlos Zubieta <carlos.zubieta@hpe.com>
2016-07-12 09:48:14 -0500

    Build and install man pages

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+echo "Install the package"
+echo "pip install --user -e $(pwd)"
 pip install --user -e .
+
+echo "Building the man pages"
+cd docs
+make man
+echo "Installing the man pages"
+cp -v _build/man/git-*.1 ${HOME}/.local/share/man/man1/
